4d0f82b746da879cbf490b5ef70cf28ee060734d
royalsis <sisaygd177@gmail.com>
2022-12-09 14:48:57 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,16 +6,17 @@
  * followed by a new line
  *
  * Return: Always 0 (Success)
- *
 */
 
 int main(void)
+
 {
 		char ch;
-		for (ch = 'a'; ch <= 'z'; ch++)
+for : (ch = 'a'; ch <= 'z'; ch++)
+
 		{
 			putchar(ch);
-			}
+		}
 		putchar('\n');
 		return (0);
 }
