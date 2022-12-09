dbe16e713ea692dc9e4f7cc7f919a75092c39f47
royalsis <sisaygd177@gmail.com>
2022-12-09 14:53:59 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,12 +11,21 @@
 int main(void)
 
 {
+
 		char ch;
+
+
+
 for : (ch = 'a'; ch <= 'z'; ch++)
 
-		{
-			putchar(ch);
-		}
-		putchar('\n');
-		return (0);
+					{
+
+								putchar(ch);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
 }
