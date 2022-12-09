7690120f7cb75466df46accfa808abcd14838586
royalsis <sisaygd177@gmail.com>
2022-12-09 03:55:05 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-/*
- * main - Entry point a line of code using puts
+/**
+ * main - Entry point
  *
- * Return: 0
- *
+ * Return: Always 0 (success)
  */
+
 int main(void)
 
 {
-
 	puts("/"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
