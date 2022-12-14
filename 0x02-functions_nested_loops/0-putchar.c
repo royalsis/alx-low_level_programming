703a89f9763b84fb276ddb4438c4167b70dc69cc
royalsis <sisaygd177@gmail.com>
2022-12-14 00:45:00 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include main.h
+
+/**
+ * main program print _puchar.c
+ * with out using global standard sign
+ * Return: 0
+ *
+*/
+
+int main(void)
+{
+	int char(_putchar = _putchar);
+
+	_putchar("putchar\n");
+return (0);
+}
