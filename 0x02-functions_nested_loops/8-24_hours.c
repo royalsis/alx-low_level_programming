19ef55bcc71afc67f5d02b072be12c89b004c925
royalsis <sisaygd177@gmail.com>
2022-12-15 01:21:09 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,59 +1,63 @@
 #include "main.h"
 
-/**
- * times_table - prints the 9 times table, starting with 0
-*/
 
-void times_table(void)
-{
 
-	int i, j, k;
 
-	for (i = 0; i < 10; i++)
 
-	{
+	/**
+	 *
+	 * 	 * jack_bauer - prints every minute of the day of Jack Bauer
+	 *
+	 * 	 	 * starting from 00:00 to 23:59
+	 *
+	 * 	 	 	 */
 
-		for (j = 0; j < 10; j++)
+	void jack_bauer(void)
 
 		{
 
-			k = j * i;
+					int i, j;
 
-			if (j == 0)
 
-					{
 
-						_putchar(k + '0');
 
-					}
 
-			if (k < 10 && j != 0)
+							i = 0;
 
-			{
 
-				_putchar(',');
 
-				_putchar(' ');
 
-				_putchar(' ');
 
-				_putchar(k + '0');
+									while (i < 24)
 
-			} else if (k >= 10)
+												{
 
-			{
+																j = 0;
 
-				_putchar(',');
+																			while (j < 60)
 
-				_putchar(' ');
+																							{
 
-				_putchar((k / 10) + '0');
+																												_putchar((i / 10) + '0');
 
-				_putchar((k % 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
+																																_putchar((i % 10) + '0');
 
+																																				_putchar(':');
+
+																																								_putchar((j / 10) + '0');
+
+																																												_putchar((j % 10) + '0');
+
+																																																_putchar('\n');
+
+																																																				j++;
+
+																																																							}
+
+																						i++;
+
+																								}
+
+										}
 
 
