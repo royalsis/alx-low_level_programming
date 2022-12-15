4edfe5f8e99d8b5af61052aca8dda3dc30f2e8c4
royalsis <sisaygd177@gmail.com>
2022-12-15 02:06:57 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,47 +1,77 @@
 #include "main.h"
-/**
- * times_table - prints the 9 times table, starting with 0
- *
-*/
 
-void times_table(void)
-{
-	int i, j, k;
 
-	for (i = 0; i < 10; i++)
 
-	{
+	/**
+	 *
+	 * 	 * times_table - prints the 9 times table, starting with 0
+	 *
+	 * 	 	 */
 
-		for (j = 0; j < 10; j++)
+	void times_table(void)
 
 		{
 
-			k = j * i;
+					int i, j, k;
 
-			if (j == 0)
 
-			{
-				_putchar(k + '0');
-			}
 
-			if (k < 10 && j != 0)
 
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
 
-			} else if (k >= 10)
+							for (i = 0; i < 10; i++)
 
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
-}
+										{
+
+														for (j = 0; j < 10; j++)
+
+																		{
+
+																							k = j * i;
+
+																											if (j == 0)
+
+																																{
+
+																																						_putchar(k + '0');
+
+																																										}
+
+
+
+
+
+																															if (k < 10 && j != 0)
+
+																																				{
+
+																																										_putchar(',');
+
+																																															_putchar(' ');
+
+																																																				_putchar(' ');
+
+																																																									_putchar(k + '0');
+
+																																																													} else if (k >= 10)
+
+																																																																		{
+
+																																																																								_putchar(',');
+
+																																																																													_putchar(' ');
+
+																																																																																		_putchar((k / 10) + '0');
+
+																																																																																							_putchar((k % 10) + '0');
+
+																																																																																											}
+
+																																																																}
+
+																	_putchar('\n');
+
+																			}
+
+								}
+
 
