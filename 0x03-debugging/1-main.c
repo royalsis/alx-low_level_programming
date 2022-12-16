114cfd23e2ae97e0108537435f9622608be5b297
royalsis <sisaygd177@gmail.com>
2022-12-16 12:01:30 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,14 +2,12 @@
 
 /**
  * main - causes an infinite loop
- *
  * Return: 0
 */
 int main(void)
 {
 	int i;
-
-
+	
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
@@ -22,8 +20,5 @@ int main(void)
 	 */
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
-
 }
-
