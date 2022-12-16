8838a542fc02a66c7034f77614d6315e581775d7
royalsis <sisaygd177@gmail.com>
2022-12-16 01:38:25 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: Variable text
+ *
+ * Return: Always 0.
+ *
+*/
+
+int _isupper(int c)
+{
+
+	if ((c >= 65) && (c <= 90))
+
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
